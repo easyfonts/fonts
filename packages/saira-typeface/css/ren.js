c9242c25d3c39b5b8d8af00076ecc21b86795b35
jacobbogers <jkfbogers@gmail.com>
2019-06-10 14:22:21 +0200

    documentation done

--- packages/saira-typeface/css/ren.js
+++ packages/saira-typeface/css/ren.js
@@ -7,5 +7,14 @@ for (const fileName of allFiles) {
         fs.renameSync(fileName, newFile);
         console.log(fileName, newFile);
     }
-    console.log("|<span style=\"font-face: monospace;\">import '@easyfonts/saira-typeface/css/"+fileName.replace('.css','')+"';</span>|   "+fileName+"      |");
+    const name = fileName.replace('.css', '');
+    const col1 = `<span style="font-face: monospace;">import @easyfonts/saira-typeface/css/${name}</span>`;
+    const matched = name.match(/^([^0-9]+)([0-9]+)([^0-9]+)?$/);
+    const reg = /\-/g;
+    if (matched) {
+        const familyName = matched[1].replace(reg,' ').trim();
+        const size = matched[2].replace(reg, ' ').trim();
+        const lang = matched[3] ? matched[3].replace(reg, ' ').trim() : 'all';
+        console.log(`|${col1}|${familyName}|${lang}|${size}|`);
     }
+}
